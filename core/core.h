abf592a764634c76dc6b68e4518ade9b0e13f5f4
viylouu <131220600+viylouu@users.noreply.github.com>
2025-11-29 02:15:41 -0700

    alloc

--- core/core.h
+++ core/core.h
@@ -20,7 +20,26 @@
     } while (0)
 #endif
 
-#ifdef cal_unused
+#ifndef cal_unused
     #define cal_unused(var) \
         (void)(var)
 #endif
+
+#ifndef cal_alloc
+    #include <stdlib.h>
+    #define cal_alloc(count, type) \
+        malloc(sizeof(type) * (count))
+#endif
+
+#ifndef cal_realloc
+    #include <stdlib.h>
+    #define cal_realloc(prev, new_count, type) \
+        realloc((prev), sizeof(type) * (new_count))
+#endif
+
+#ifndef cal_free
+    #include <stdlib.h>
+    #define cal_free(var) \
+        free(var)
+#endif
+
